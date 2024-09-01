b5dd9ff26cf8c73a56de5cbf22fb70efc073e75d
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-01 17:20:38 +0900

    create custom backend authentication that enable user to login with username or passowrd

---
+++ accounts/auth_backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class UsernameOrEmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        try:
+            # get user with username or email
+            if "@" in username:
+                user = UserModel.objects.get(email=username)
+            else:
+                user = UserModel.objects.get(username=username)
+        except UserModel.DoesNotExist:
+            return None
+
+        # password validation
+        if user.check_password(password):
+            return user
+
+    def get_user(self, user_id):
+        UserModel = get_user_model()
+        try:
+            return UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
