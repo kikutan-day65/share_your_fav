830c5da071a14b41319c6004d5fa83fd7c09208d
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-01 17:23:57 +0900

    add login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
+from .forms import CustomAuthenticationForm
+
+
 # Create your views here.
+class MyLoginView(LoginView):
+    form_class = CustomAuthenticationForm
+    template_name = "registration/login.html"
