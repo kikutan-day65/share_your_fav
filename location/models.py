a5fbd5962530e3c57a0744ce2e2c44580fbd3a13
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-03 00:42:35 +0900

    add location model

--- location/models.py
+++ location/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    description = models.TextField(blank=True)
+    like_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
