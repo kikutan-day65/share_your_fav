b4cbaa826581a39c97c6c9c733954bb96c100474
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-02 20:28:52 +0900

    define user signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,16 @@
-from django.contrib.auth.forms import AuthenticationForm
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from .models import CustomUser
 
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label="Username or Email", max_length=255)
+
+
+class SignUpForm(UserCreationForm):
+    usable_password = None
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "password1", "password2")
