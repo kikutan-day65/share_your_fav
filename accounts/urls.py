a4fe1d739ef07d1e06298c906462331f997939ee
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-02 20:30:30 +0900

    create sigup page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MyLoginView, MyLogoutView
+from .views import MyLoginView, MyLogoutView, SignUpView
 
 app_name = "accounts"
 urlpatterns = [
     path("login/", MyLoginView.as_view(), name="login"),
     path("logout/", MyLogoutView.as_view(), name="logout"),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
