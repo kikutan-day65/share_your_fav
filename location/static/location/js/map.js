7c409a2055fd35fefac96dba9e1af9bfcbd47579
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-05 20:28:58 +0900

    add class to intialize location

--- location/static/location/js/map.js
+++ location/static/location/js/map.js
@@ -1,7 +1,22 @@
-var map = L.map("map", {
-    center: [51.505, -0.09],
-    zoom: 13
-});
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+class LeafletMap {
+    constructor(elementId, latitude, longitude, zoom) {
+        this.elementId = elementId;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.zoom = zoom;
+    };
+
+    initializeMap() {
+        var map = L.map(this.elementId, {
+            center: [this.latitude, this.longitude],
+            zoom: this.zoom
+        });
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
+    };
+};
+
+var mapInstance = new LeafletMap("map", 35.4437, 139.6380, 13);
+intialMap = mapInstance.initializeMap();
