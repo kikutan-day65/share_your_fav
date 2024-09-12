3d23f63dfc2608ddec746a49269e01e51bd52f70
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-12 23:46:18 +0900

    add location form view not implemented post yet

--- location/views.py
+++ location/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views import View
 
+from .forms import LocationForm
+
 
 # Create your views here.
 class Map(View):
     def get(self, request):
-        return render(request, "map.html")
+        return render(request, "location/map.html")
+
+
+class LocationFormView(View):
+    def get(self, request):
+        form = LocationForm()
+        context = {"form": form}
+        return render(request, "location/location_form.html", context)
+
+    def post(self, request):
+        form = LocationForm(request.POST)
+        if not form.is_valid():
+            context = {"form": form}
+            return render(request, "location/location_form.html", context)
