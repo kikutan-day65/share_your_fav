240ebe5d76d3c098c5f0b318138620c9ee887ce5
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-14 12:54:31 +0900

    add location form url

--- location/urls.py
+++ location/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import LocationView, Map
+from .views import LocationFormView, Map
 
 app_name = "map"
 urlpatterns = [
     path("", Map.as_view(), name="map"),
-    path("location-form/", LocationView.as_view(), name="location_form"),
+    path("location-form/", LocationFormView.as_view(), name="location_form"),
 ]
