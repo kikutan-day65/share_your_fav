26c9446b3c0c683d03b29f0a34009b9d6de9c577
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-25 00:05:07 +0900

    add url to display location data detail

--- location/urls.py
+++ location/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import LocationFormView, Map
+from .views import LocationDataView, LocationFormView, Map
 
 app_name = "map"
 urlpatterns = [
     path("", Map.as_view(), name="map"),
     path("location-form/", LocationFormView.as_view(), name="location_form"),
+    path("location-data/<int:pk>/", LocationDataView.as_view(), name="location_data"),
 ]
