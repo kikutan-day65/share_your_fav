c0a0157c8767613c8edbe500551858136b0c8210
haruto.ubuntu <haruto-cs@outlook.com>
2024-09-27 19:30:11 +0900

    add like model

--- location/models.py
+++ location/models.py
@@ -8,11 +8,20 @@ class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     description = models.TextField(blank=True)
-    like_count = models.IntegerField(default=0)
-    dislike_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return str(self.name)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "location")
+
+    def __str__(self) -> str:
+        return f"{self.user} liked {self.location}"
