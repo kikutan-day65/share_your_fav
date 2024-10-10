a5e66aae1a4a2dedb2e430e8976a6a0753eb9be2
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-10 20:48:49 +0900

    add user register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
+from .models import CustomUser
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label="Username or Email")
+
+
+class CustomUserRegisterForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "first_name", "last_name", "password"]
