887d223d2039068ad3b128e984ff6a4fb645fb2a
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-10 20:47:05 +0900

    add user register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from .forms import CustomAuthenticationForm, CustomUserRegisterForm
+from .models import CustomUser
+
 
 # Create your views here.
+class UserRegisterView(CreateView):
+    model = CustomUser
+    form_class = CustomUserRegisterForm
+    template_name = "accounts/user_register_form.html"
+    # success_url = "accounts:accounts_home"
