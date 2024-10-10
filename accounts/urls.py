e3a8a01d43d665414d6fdaa5ba1c2cbd0794fbfc
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-10 20:47:51 +0900

    add url for user register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from .views import UserRegisterView
+
+app_name = "accounts"
 urlpatterns = [
-    
+    # path("accounts-home/", view_name.as_view(), name="accounts_home"),
+    path(
+        "user-register-form/",
+        UserRegisterView.as_view(),
+        name="user_register_form.html",
+    ),
 ]
