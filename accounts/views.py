c08467aa5626b457c18449f33bba3cb8d191b367
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-11 20:56:36 +0900

    add user detail view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, TemplateView
 
 from .forms import CustomAuthenticationForm, CustomUserRegisterForm
 from .models import CustomUser
@@ -10,4 +11,10 @@ class UserRegisterView(CreateView):
     model = CustomUser
     form_class = CustomUserRegisterForm
     template_name = "accounts/user_register_form.html"
-    # success_url = "accounts:accounts_home"
+    success_url = reverse_lazy("accounts:register_success_page")
+
+
+class UserDetailView(DetailView):
+    model = CustomUser
+    template_name = "accounts/user_detail.html"
+    context_object_name = "user_detail"
