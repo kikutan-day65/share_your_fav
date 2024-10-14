d94f3ffef1751c9e3773b43f3afe8afd24ab23a4
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-14 12:08:25 +0900

    enable to confirm the password

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,3 +17,13 @@ class CustomUserRegisterForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ["username", "email", "first_name", "last_name"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and (password != confirm_password):
+            raise forms.ValidationError("Passwords do not match.")
+
+        return cleaned_data
