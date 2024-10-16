ae025c449d3e36c4f50ecc4645e065368aa9b335
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-16 18:40:26 +0900

    add map.js

---
+++ location/static/location/js/map.js
@@ -0,0 +1,6 @@
+var map = L.map("map").setView([35.6764, 139.65], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
