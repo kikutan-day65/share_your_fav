1ac74a1fb34cb1d51c31705236496536f11afea4
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-19 11:48:49 +0900

    add Location model and migrate it

--- location/models.py
+++ location/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=150)
+    latitude = models.FloatField(unique=True)
+    longitude = models.FloatField(unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
