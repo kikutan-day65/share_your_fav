373a6195a6bda3b48a12010a51483ad077cc9650
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-24 20:22:22 +0900

    add location create view

--- location/views.py
+++ location/views.py
@@ -1,7 +1,21 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+
+from .forms import LocationForm
 
 
 # Create your views here.
 class MapView(TemplateView):
     template_name = "location/map.html"
+
+
+class LocationCreateView(LoginRequiredMixin, CreateView):
+    form_class = LocationForm
+    template_name = "location/location_create_form.html"
+    success_url = reverse_lazy("location:map")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
