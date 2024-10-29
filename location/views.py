8538caf18c13ee37e3f7139d96610fc798ee6204
haruto.ubuntu <haruto-cs@outlook.com>
2024-10-30 00:52:35 +0900

    add location update view

--- location/views.py
+++ location/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, TemplateView
+from django.views.generic import CreateView, DetailView, TemplateView, UpdateView
 
 from .forms import LocationForm
 from .models import Location
@@ -25,3 +25,12 @@ class LocationCreateView(LoginRequiredMixin, CreateView):
 class LocationDetailView(DetailView):
     template_name = "location/location_detail.html"
     model = Location
+
+
+class LocationUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "location/location_update_form.html"
+    model = Location
+    fields = ["name", "description"]
+
+    def get_success_url(self):
+        return reverse_lazy("location:location_detail", kwargs={"pk": self.object.pk})
