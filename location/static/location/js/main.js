3d68529118bc78dcf3371b2b344a91632e91a830
haruto.ubuntu <haruto-cs@outlook.com>
2024-11-07 19:34:38 +0900

    enable to access locations data in js

--- location/static/location/js/main.js
+++ location/static/location/js/main.js
@@ -2,3 +2,17 @@ import { MapManager, LocationManager } from "./geoModule.js";
 
 const mapController = new MapManager("map", 35.466, 139.6221, 13);
 mapController.initializeMap();
+
+// locations-dataスクリプトタグからデータを取得
+const locationsDataString = JSON.parse(
+    document.getElementById("locations-data").textContent
+);
+
+const locationsData = JSON.parse(locationsDataString);
+
+// ループして.nameにアクセスする例
+locationsData.forEach((location) => {
+    console.log(location.fields.name); // nameを出力
+});
+
+// const locationsObj = new LocationManager(locationsData);
