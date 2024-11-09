24b188ff6adfc6e7ac867508b6f7a5edbb87ec20
haruto.ubuntu <haruto-cs@outlook.com>
2024-11-09 14:26:57 +0900

    add addMarkers method

--- location/static/location/js/geoModule.js
+++ location/static/location/js/geoModule.js
@@ -4,6 +4,7 @@ class MapManager {
             [initLatitude, initLongitude],
             zoomLevel
         );
+        this.currentMarker = null;
     }
 
     initializeMap() {
@@ -14,11 +15,26 @@ class MapManager {
         }).addTo(this.map);
     }
 
-    addMarkers(locations) {
+    showMarkers(locations) {
         locations.forEach((location) => {
             L.marker([location.fields.latitude, location.fields.longitude])
                 .addTo(this.map)
                 .bindPopup(location.fields.name);
+        });
+    }
+
+    addMarkers() {
+        this.map.on("click", (event) => {
+            const { lat, lng } = event.latlng;
+
+            if (this.currentMarker) {
+                this.map.removeLayer(this.currentMarker);
+            }
+
+            this.currentMarker = L.marker([lat, lng]).addTo(this.map);
+            this.currentMarker
+                .bindPopup(`Latitude: ${lat}, Longitude: ${lng}`)
+                .openPopup();
         });
     }
 }
