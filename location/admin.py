8ba8a8598a058da80fc203770ce1ca4eb5a21790
haruto.ubuntu <haruto-cs@outlook.com>
2024-11-27 20:48:39 +0900

    register photo model to admin page

--- location/admin.py
+++ location/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Location
+from .models import Location, Photo
 
 # Register your models here.
 admin.site.register(Location)
+admin.site.register(Photo)
